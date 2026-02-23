14bf8fda07ba96bedc29ded6d9a61c5b94a0698d
JAEMIN OH <86649554+ojm6135@users.noreply.github.com>
2026-02-24 03:55:07 +0900

    [Silver II] Title: 마라탕 재료 고르기, Time: 96 ms, Memory: 14340 KB -BaekjoonHub

--- 백준/Silver/28447. 마라탕 재료 고르기/마라탕 재료 고르기.java
+++ 백준/Silver/28447. 마라탕 재료 고르기/마라탕 재료 고르기.java
@@ -5,7 +5,6 @@ public class Main {
     static int N;
     static int M;
     static int[][] materials;
-    static boolean[] visited;
 
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
@@ -14,8 +13,7 @@ public class Main {
         N = Integer.parseInt(st.nextToken());
         M = Integer.parseInt(st.nextToken());
         materials = new int[N][N];
-        visited = new boolean[N];
-        int max = Integer.MIN_VALUE;
+        int res = Integer.MIN_VALUE;
 
         for (int i = 0; i < N; i++) {
             st = new StringTokenizer(br.readLine());
@@ -31,40 +29,34 @@ public class Main {
             return;
         }
 
-        for (int i = 0; i < N; i++) {
-            max = Math.max(max, dfs(new Stack<>(), i, 0));
-            Arrays.fill(visited, false);
+        for (int i = 0; i <= N - M; i++) {
+            res = Math.max(res, dfs(new Stack<>(), i + 1, i, 0));
         }
 
-        System.out.println(max);
+        System.out.println(res);
 
         br.close();
     }
 
-    static int dfs(Stack<Integer> stack, int picked, int score) {
+    static int dfs(Stack<Integer> stack, int start, int picked, int score) {
         for (int m : stack) {
             score += materials[picked][m];
         }
 
         stack.push(picked);
-        visited[picked] = true;
 
         if (stack.size() == M) {
             stack.pop();
-            visited[picked] = false;
             return score;
         }
 
         int max = Integer.MIN_VALUE;
 
-        for (int i = 0; i < N; i++) {
-            if (!visited[i]) {
-                max = Math.max(max, dfs(stack, i, score));
-            }
+        for (int i = start; i < N; i++) {
+            max = Math.max(max, dfs(stack, i + 1, i, score));
         }
 
         stack.pop();
-        visited[picked] = false;
 
         return max;
     }
